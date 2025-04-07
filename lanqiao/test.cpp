1a57394c2197e67276f7a8c4bf51d3547847f902
hupax <supanx11@gmail.com>
2025-04-07 15:02:40 +0800

    30

---
+++ lanqiao/test.cpp
@@ -0,0 +1,17 @@
+//
+// Created by hupax on 25-4-5.
+//
+#include <bits/stdc++.h>
+#define gc getchar_unlocked()
+using namespace std;
+
+int main() {
+    int a, b;
+    char c;
+    cin>>a;
+//    gc;
+    c=gc;
+    cout<<a<<c;
+    
+    return 0;
+}
