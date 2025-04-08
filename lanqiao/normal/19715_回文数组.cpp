2bc536264d7bdb0daaf7ab5edf40a44332dfafaf
hupax <supanx11@gmail.com>
2025-04-08 23:04:19 +0800

    30

--- lanqiao/normal/19715_回文数组.cpp
+++ lanqiao/normal/19715_回文数组.cpp
@@ -2,22 +2,39 @@
 // Created by hupax on 25-4-7.
 //
 #include <iostream>
-#include <cmath>
 using namespace std;
-
 const int N=1e5+5;
-int a[N];
-
+int n, a[N], df[N];
+int ans=0;
 int main() {
-    int n;
-    cin >> n;
-    for (int i=1; i <= n; i++) cin >> a[i];
-    
-    int ans=0;
-    for (int i=1; i <= n/2; i++) {
-        ans+=abs(a[i]-a[n-i+1]);
+    cin>>n;
+    for (int i=1; i<=n; i++) cin>>a[i];
+    for (int i=1; i<=n/2; i++) df[i]=a[n-i+1]-a[i];
+    for (int i=1; i<=n/2; i++) {
+        if (df[i]>0 && df[i+1]>0) {
+            if (df[i]<df[i+1]) {
+                ans+=df[i];
+                i++;
+            }
+            else {
+                ans+=df[i];
+                df[i+1]-=df[i];
+            }
+        }
+        else if (df[i]<0 && df[i+1]<0) {
+            if (df[i]<df[i+1]) {
+                ans+=-df[i];
+                i++;
+            }
+            else {
+                ans+=-df[i];
+                df[i+1]-=df[i];
+            }
+        }
+        else ans+=abs(df[i]);
     }
-    cout << ans << endl;
+    cout<<ans;
+    
     return 0;
 }
 
